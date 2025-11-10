7b93224208ca5832b6aac1d4edda8903fd036288
Maryna Kulikova <plohoeimja@gmail.com>
2025-11-10 22:43:55 +0300

    feat: solve tasks Transform arr, Mineseeper

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,43 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+
+  constructor(direct = true) {
+    this.direct = direct;
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  _process(text, key, encrypt) {
+    if (!text || !key) throw new Error('Incorrect arguments!');
+    
+    text = text.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+    let j = 0;
+
+    for (let i = 0; i < text.length; i++) {
+      const char = text[i];
+      if (/[A-Z]/.test(char)) {
+        const shift = key[j % key.length].charCodeAt(0) - 65;
+        const code = char.charCodeAt(0) - 65;
+        const newCode = encrypt ? 
+          (code + shift) % 26 : 
+          (code - shift + 26) % 26;
+        result += String.fromCharCode(newCode + 65);
+        j++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.direct ? result : [...result].reverse().join('');
+  }
+
+  encrypt(text, key) {
+    return this._process(text, key, true);
+  }
+
+  decrypt(text, key) {
+    return this._process(text, key, false);
   }
 }
 
